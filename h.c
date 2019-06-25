ab310dab6ae87041ba4e755d72e26051d8bd566f
Franz Scheerer <scheerer.software@gmail.com>
2019-06-25 18:47:14 +0200

    SPRITZ240

--- h.c
+++ h.c
@@ -1,274 +1,67 @@
 #include <stdio.h>
-#include <stddef.h>
-#include <string.h>
 
-#define N 256
-#define NN 512
-#define N2 128
+#define SWAP(A,B) tmp = A; A = B; B = tmp;
 
-typedef struct {
-    unsigned char a, i, j, k, w, z, s[N];
-} State;
+int a = 0, i = 0, j = 0, w = 1, s[256];
+int tmp;
 
-#define LOW(B)  ((B) & 0xf)
-#define HIGH(B) ((B) >> 4)
-
-static void
-initialize_state(State *state)
+void update()
 {
-    unsigned int v;
+    i = (i + w) % 256;
+    j = s[(j + s[i]) % 256];
+    SWAP(s[i], s[j])
+}
 
-    for (v = 0; v < N; v++) {
-        state->s[v] = (unsigned char) v;
+void absorb_nibble(int x)
+{
+    if (a == 240){ 
+       for (int v = 0; v < 256; v++) 
+           update();
+       w = (w + 2) % 256;
+       a = 0;
     }
-    state->a = state->i = state->j = state->k = 0;
-    state->w = 1;
-    state->z = 0;
+    SWAP(s[a], s[x+240])
+    a++;
 }
 
-static void
-update(State *state)
+void absorb_byte(int b)
 {
-    unsigned char t,y;
-
-    state->i += state->w;
-    y = state->j + state->s[state->i];
-    state->j = state->k + state->s[y];
-    state->k = state->i + state->k 
-             + state->s[state->j];
-    t = state->s[state->i];
-    state->s[state->i] = state->s[state->j];
-    state->s[state->j] = t;
+    absorb_nibble(b % 16);
+    absorb_nibble(b / 16);
 }
 
-static unsigned char
-output(State *state)
+int output()
 {
-    const unsigned char y1 = state->z + state->k;
-    const unsigned char x1 = state->i + state->s[y1];
-    const unsigned char y2 = state->j + state->s[x1];
-
-    state->z = state->s[y2];
-
-    return state->z;
+    update();
+    return s[j];
 }
 
-static void
-crush(State *state)
+void squeeze(char *out, size_t outlen)
 {
-    unsigned char v, x1, x2, y;
-
-    for (v = 0; v < N2; v++) {
-        y = (N - 1) - v;
-        x1 = state->s[v];
-        x2 = state->s[y];
-        if (x1 > x2) {
-            state->s[v] = x2;
-            state->s[y] = x1;
-        } else {
-            state->s[v] = x1;
-            state->s[y] = x2;
-        }
-    }
+   if (a != 0){ 
+       for (int v = 0; v < 256; v++) 
+           update();
+       w = (w + 2) % 256;
+       a = 0;
+   }    
+   for (int v = 0; v < outlen; v++) 
+       out[v] = output();
 }
-
-static void
-whip(State *state)
-{
-    for (int v = 0; v < NN; v++) {
-        update(state);
-    }
-    state->w += 2;
-}
-
-static void
-shuffle(State *state)
-{
-    whip(state);
-//    crush(state);
-//    whip(state);
-//    crush(state);
-//    whip(state);
-    state->a = 0;
-}
-
-static void
-absorb_stop(State *state)
-{
-    if (state->a == N2) {
-        shuffle(state);
-    }
-    state->a++;
-}
-
-static void
-absorb_nibble(State *state, const unsigned char x)
-{
-    unsigned char t, y;
-
-    if (state->a == N2) {
-        shuffle(state);
-    }
-    y = N2 + x;
-    t = state->s[state->a];
-    state->s[state->a] = state->s[y];
-    state->s[y] = t;
-    state->a++;
-}
-
-static void
-absorb_byte(State *state, const unsigned char b)
-{
-    absorb_nibble(state, LOW(b));
-    absorb_nibble(state, HIGH(b));
-}
-
-static void
-absorb(State *state, const unsigned char *msg, size_t length)
-{
-    size_t v;
-
-    for (v = 0; v < length; v++) {
-        absorb_byte(state, msg[v]);
-    }
-}
-
-static unsigned char
-drip(State *state)
-{
-    if (state->a > 0) {
-        shuffle(state);
-    }
-    update(state);
-
-    return output(state);
-}
-
-static void
-squeeze(State *state, unsigned char *out, size_t outlen)
-{
-    if (state->a > 0) {
-        shuffle(state);
-    }
-    for (size_t v = 0; v < outlen; v++) {
-        out[v] = drip(state);
-    }
-}
-
-static void
-key_setup(State *state, const unsigned char *key, size_t keylen)
-{
-    initialize_state(state);
-    absorb(state, key, keylen);
-}
-
-int
-spritz_hash(unsigned char *out, size_t outlen,
-            const unsigned char *msg, size_t msglen)
-{
-    State         state;
-    unsigned char r;
-
-    if (outlen > 255) {
-        return -1;
-    }
-    r = (unsigned char) outlen;
-    initialize_state(&state);
-    absorb(&state, msg, msglen);
-    absorb_stop(&state);
-    absorb(&state, &r, 1U);
-    squeeze(&state, out, outlen);
-
-    return 0;
-}
-
-int
-spritz_stream(unsigned char *out, size_t outlen,
-              const unsigned char *key, size_t keylen)
-{
-    State state;
-
-    initialize_state(&state);
-    absorb(&state, key, keylen);
-    squeeze(&state, out, outlen);
-
-    return 0;
-}
-
-int
-spritz_encrypt(unsigned char *out, const unsigned char *msg, size_t msglen,
-               const unsigned char *nonce, size_t noncelen,
-               const unsigned char *key, size_t keylen)
-{
-    State  state;
-    size_t v;
-
-    key_setup(&state, key, keylen);
-    absorb_stop(&state);
-    absorb(&state, nonce, noncelen);
-    for (v = 0; v < msglen; v++) {
-        out[v] = msg[v] + drip(&state);
-    }
-
-    return 0;
-}
-
-int
-spritz_decrypt(unsigned char *out, const unsigned char *c, size_t clen,
-               const unsigned char *nonce, size_t noncelen,
-               const unsigned char *key, size_t keylen)
-{
-    State  state;
-    size_t v;
-
-    key_setup(&state, key, keylen);
-    absorb_stop(&state);
-    absorb(&state, nonce, noncelen);
-    for (v = 0; v < clen; v++) {
-        out[v] = c[v] - drip(&state);
-    }
-
-    return 0;
-}
-
-int
-spritz_auth(unsigned char *out, size_t outlen,
-            const unsigned char *msg, size_t msglen,
-            const unsigned char *key, size_t keylen)
-{
-    State         state;
-    unsigned char r;
-
-    if (outlen > 255) {
-        return -1;
-    }
-    r = (unsigned char) outlen;
-    key_setup(&state, key, keylen);
-    absorb_stop(&state);
-    absorb(&state, msg, msglen);
-    absorb_stop(&state);
-    absorb(&state, &r, 1U);
-    squeeze(&state, out, outlen);
-
-    return 0;
-}
-
 
 int main(){
-  int ii,c;
-  State state;
-
+  int c;
   unsigned char out[32];
   
-  printf("compile source: gcc h.c :\n");
-  printf("cat file | ./a.out:\n\n");
-  printf("hash (SPRITZ-256):\n");
-  initialize_state(&state);
+  printf("hash ");
+
+  for (int v = 0; v < 256; v++) 
+      s[v] = v;
+
   while ( (c = fgetc(stdin)) != -1 ){
-    absorb_byte(&state, (unsigned char) c);
+    absorb_byte(c);
   }
-  squeeze(&state, out, 32);
-  for (ii=0; ii<32; ii++)  
-     printf("%02x", out[ii]);
+  squeeze(out, 32);
+  for (int ii=0; ii<32; ii++)  
+     printf("%x", out[ii]);
   printf("\n");
 }
