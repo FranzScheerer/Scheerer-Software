c7c16c0d9cda0ecd86719c9ce2baee683a661429
Franz Scheerer <scheerer.software@gmail.com>
2019-06-30 18:57:37 +0200

    With 800 bits as number
    knoppix@Microknoppix:~$ python  h240.py h240.py
    SPRITZ:  169188274079c8e447eda75a13249be7ec2e7676394f46e9d9f1a0e91c91f2
    h800:  6184354651204834012608834793755457283270036422306994546010508572444681133183397546144918628756106887216884458255211948755852673868891479141944064340300801342270970652732834165301737731678898172154978333982518065949362435176617117076688814428

--- h240.py
+++ h240.py
@@ -1,51 +1,52 @@
 import sys
 
-def update():
-    global a,i,j,w,s
-    i = (i + w) % 256
-    j = s[(j + s[i]) % 256]
-    s[i], s[j] = s[j], s[i]
+def update_spritz():
+    global a_spritz,i_spritz,j_spritz,w_spritz,s_spritz
+    i_spritz = (i_spritz + w_spritz) % 256
+    j_spritz = s_spritz[(j_spritz + s_spritz[i_spritz]) % 256]
+    s_spritz[i_spritz], s_spritz[j_spritz] = s_spritz[j_spritz], s_spritz[i_spritz]
 
-def output():
-    global a,i,j,w,s
-    update()
-    return s[j]
+def output_spritz():
+    global a_spritz,i_spritz,j_spritz,w_spritz,s_spritz
+    update_spritz()
+    return s_spritz[j_spritz]
 
-def shuffle():
-    global a,i,j,w,s
+def shuffle_spritz():
+    global a_spritz,i_spritz,j_spritz,w_spritz,s_spritz
     for v in range(256):
-        update()    
-    w = (w + 2) % 256
-    a = 0
+        update_spritz()    
+    w_spritz = (w_spritz + 2) % 256
+    a_spritz = 0
 
-def absorb_nibble(x):
-    global a,i,j,w,s
-    if a == 240:
-        shuffle()
-    s[a], s[240 + x] = s[240 + x], s[a]
-    a = a + 1
+def absorb_nibble_spritz(x):
+    global a_spritz,i_spritz,j_spritz,w_spritz,s_spritz
+    if a_spritz == 240:
+        shuffle_spritz()
+    s_spritz[a_spritz], s_spritz[240 + x] = s_spritz[240 + x], s_spritz[a_spritz]
+    a_spritz = a_spritz + 1
 
-def absorb_byte(b):
-    absorb_nibble(b % 16)
-    absorb_nibble(b / 16)
+def absorb_byte_spritz(b):
+    absorb_nibble_spritz(b % 16)
+    absorb_nibble_spritz(b / 16)
 
-def squeeze(out, outlen):
-    global a,i,j,w,s
-    if a != 0:
-        shuffle()
+def squeeze_spritz(out, outlen):
+    global a_spritz,i_spritz,j_spritz,w_spritz,s_spritz
+    if a_spritz != 0:
+        shuffle_spritz()
     for v in range(outlen):
-        out.append(output())
+        out.append(output_spritz())
   
 f = open(sys.argv[1], 'r')
 msg = f.read()
-s = []   
-a = i = j = 0
-w = 1
+#msg = 'franz'
+s_spritz = []   
+a_spritz = i_spritz = j_spritz = 0
+w_spritz = 1
 for v in range(256):
-   s.append(v)
+   s_spritz.append(v)
 
 for c in msg:
-  absorb_byte(ord(c))
+  absorb_byte_spritz(ord(c))
 
 def num2hextxt(x):
   res = ''
@@ -61,8 +62,19 @@ def bin2num(x):
     res = (res<<8) ^ ord(c)
   return res
 
+def h800(x):
+  global a_spritz, i_spritz, j_spritz, w_spritz, s_spritz
+  out = []
+  for c in x:
+    absorb_byte_spritz(ord(c))
+  squeeze_spritz(out, 100)
+  res = 0
+  for n in out:
+    res = (res << 8) + n
+  return res
+  
 out = []
-squeeze(out, 32)
+squeeze_spritz(out, 32)
 otx = ''
 for n in out:
   if len(num2hextxt(n)) > 1 and n < 16: 
@@ -71,3 +83,4 @@ for n in out:
     otx += num2hextxt(n)
     
 print "SPRITZ: ", otx
+print "h800: ", h800('franz')
